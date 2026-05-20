a3f3a225b4b86b5c373ea62f717ad4d02de73696
KIS-IT-PRODUCTION <igorreserv18kis@gmail.com>
2026-05-20 15:01:11 +0300

    fix: update API URL to secure tunnel

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -31,7 +31,8 @@ export default function Admin() {
   const [activeTab, setActiveTab] = useState('dashboard');
 
   // const API_BASE_URL = 'https://goldstadtaktuell-backend-production.up.railway.app/api'; 
-  const API_BASE_URL = "http://49.13.148.71:3000/api";
+  // const API_BASE_URL = "http://49.13.148.71:3000/api";
+  const API_BASE_URL = 'https://ninety-eagles-invite.loca.lt/api';
   useEffect(() => {
     const token = localStorage.getItem('token');
     const role = localStorage.getItem('role');
