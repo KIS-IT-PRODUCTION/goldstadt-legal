0fb8a2585d96c25ac300135e27d130a550c819d7
KIS-IT-PRODUCTION <igorreserv18kis@gmail.com>
2026-05-20 15:04:53 +0300

    feat: aded new url api

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -32,7 +32,7 @@ export default function Admin() {
 
   // const API_BASE_URL = 'https://goldstadtaktuell-backend-production.up.railway.app/api'; 
   // const API_BASE_URL = "http://49.13.148.71:3000/api";
-  const API_BASE_URL = 'https://ninety-eagles-invite.loca.lt/api';
+  const API_BASE_URL = 'https://busy-coins-remain.loca.lt/api';
   useEffect(() => {
     const token = localStorage.getItem('token');
     const role = localStorage.getItem('role');
